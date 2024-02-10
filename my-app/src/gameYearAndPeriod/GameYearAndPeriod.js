4956ca3d1bacdff35433851a10b2bd76ed0fba36
takikomiudon <takikomiudon1743@gmail.com>
2024-02-10 23:01:36 +0900

    変なmarginを修正

--- my-app/src/gameYearAndPeriod/GameYearAndPeriod.js
+++ my-app/src/gameYearAndPeriod/GameYearAndPeriod.js
@@ -1,7 +1,7 @@
 const GameYearAndPeriod = (props) => {
     return (
         <div>
-            <h1>{props.props.year}年目第{props.props.period}期</h1>
+            <h1 style={{ margin: 0, padding: '12px' }}>{props.props.year}年目第{props.props.period}期</h1>
         </div>
     );
 }
