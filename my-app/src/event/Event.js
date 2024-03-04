4d731651578f28d6b4697d8d5ad62331d29dfd58
Maruyama <maruyama-kohei288@g.ecc.u-tokyo.ac.jp>
2024-03-04 17:52:14 +0900

    丸山担当分

--- my-app/src/event/Event.js
+++ my-app/src/event/Event.js
@@ -1,8 +1,11 @@
 const Event = () => {
+    //イベント表示(丸山)
     return (
         <div>
-        <h1>イベント</h1>
-        {/* 丸山くん イベントをここで表示 */}
+        <h1>{props.props.eventArray[props.props.eventNum]}</h1>
+        {/* 丸山くん イベントをここで表示 */
+       
+        }
         </div>
     );
 }
