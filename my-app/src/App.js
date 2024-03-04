2ca453bcca0c42556de5191b8589d7fa4fc2f755
Maruyama <maruyama-kohei288@g.ecc.u-tokyo.ac.jp>
2024-03-04 17:41:46 +0900

    丸山担当分

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,6 @@ import Start from './start/Start';
 
 function App() {
   // 4プレイヤーの所持株式、所持金をstateを用いて管理する
-  //konnitiha
   const initialPlayerState = {
     stocks: [0, 0, 0, 0, 0],
     money: 1000
@@ -32,6 +31,16 @@ function App() {
   const [isFinished, setIsFinished] = useState(false);
 
   // イベントをここに定義する(一旦5つ、株価が変動するやつ　内容はスプレッドシート参照) 丸山くん
+    const アベノミクス = [0, 2, 1, 0, 3];//スプシ4行目
+    const 金融緩和 = [0, 1, 0, 1, 2];//スプシ9行目
+    const リーマンショック = [0, -3, 0, -1, -2];//スプシ14行目
+    const 長期金利の上昇 = [0, 3, 0, 0, 0];//スプシ19行目
+    const D社赤字を発表 = [0, 0, 0, -3, 0];//スプシ24行目
+
+    const eventArray = [アベノミクス, 金融緩和, リーマンショック, 長期金利の上昇, D社赤字を発表];
+    const [eventNum, setEventNum] = useState(0);//eventArrayの何番目を表示するか
+
+    //ここまで(丸山)
 
   // プレイヤーの行動ログを記録
   // めんどいので任せます
