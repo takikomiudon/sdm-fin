bb61b612fe6aec4d07f3a8b51ce4a9268c5d7566
takikomiudon <takikomiudon1743@gmail.com>
2024-03-31 21:02:05 +0900

    イベント部分にアイコン追加

--- my-app/src/components/event/Event.tsx
+++ my-app/src/components/event/Event.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import events from "../../data/events";
+import ScienceIcon from '@mui/icons-material/Science';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import ComputerIcon from '@mui/icons-material/Computer';
+import PsychologyIcon from '@mui/icons-material/Psychology';
 
 const Event = ({ eventNum }: { eventNum: number }) => {
   return (
     <div>
       <h1>イベント</h1>
       <h2>{events[eventNum].name}</h2>
-      <p>
-        A{events[eventNum].effect[0]}
-        &nbsp;B{events[eventNum].effect[1]}
-        &nbsp;C{events[eventNum].effect[2]}
-        &nbsp;D{events[eventNum].effect[3]}
-        &nbsp;E{events[eventNum].effect[4]}
+      <p className="flex flex-row justify-evenly">
+        <ScienceIcon />{events[eventNum].effect[0]}
+        <AccountBalanceWalletIcon />{events[eventNum].effect[1]}
+        <LocalShippingIcon />{events[eventNum].effect[2]}
+        <ComputerIcon />{events[eventNum].effect[3]}
+        <PsychologyIcon />{events[eventNum].effect[4]}
       </p>
     </div>
   );
