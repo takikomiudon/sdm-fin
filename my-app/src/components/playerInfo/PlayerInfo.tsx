23d86f45e29ffcdc4388e1dae2e763c8c9fe58ec
MaeYuu555 <mae.104.yuu@gmail.com>
2024-04-15 19:22:32 +0900

    プレイヤー情報の、自分の部分だけ背景色を変えました。

--- my-app/src/components/playerInfo/PlayerInfo.tsx
+++ my-app/src/components/playerInfo/PlayerInfo.tsx
@@ -3,19 +3,38 @@ import logo from "./img/logo192.png";
 import { Player } from "../../types/player";
 
 const PlayerInfo = ({ player }: { player: Player }) => {
-  return (
-    <tr>
-      <td>
-        <img src={logo} className="h-12" alt="logo" />
-      </td>
-      <td>{player.stocks[0]}</td>
-      <td>{player.stocks[1]}</td>
-      <td>{player.stocks[2]}</td>
-      <td>{player.stocks[3]}</td>
-      <td>{player.stocks[4]}</td>
-      <td>{player.money}万円</td>
-    </tr>
-  );
+
+  if (player.name == "あなた"){
+    return (
+
+      <tr style={{ backgroundColor: '#35599c' }}>
+        <td>
+          <img src={logo} className="h-12" alt="logo" />
+        </td>
+        <td>{player.stocks[0]}</td>
+        <td>{player.stocks[1]}</td>
+        <td>{player.stocks[2]}</td>
+        <td>{player.stocks[3]}</td>
+        <td>{player.stocks[4]}</td>
+        <td>{player.money}万円</td>
+      </tr>
+    );
+  }else{
+    return (
+
+      <tr>
+        <td>
+          <img src={logo} className="h-12" alt="logo" />
+        </td>
+        <td>{player.stocks[0]}</td>
+        <td>{player.stocks[1]}</td>
+        <td>{player.stocks[2]}</td>
+        <td>{player.stocks[3]}</td>
+        <td>{player.stocks[4]}</td>
+        <td>{player.money}万円</td>
+      </tr>
+    );
+  }
 };
 
 export default PlayerInfo;
